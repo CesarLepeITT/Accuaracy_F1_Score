caebbc520b7106c63994b29a91163d46e4f37cb7
CesarLepeITT <l22212360@tijuana.tecnm.mx>
2023-11-03 12:30:02 -0700

    Actualizacion de headers y TODO's anadidos

---
+++ Accuaracy.cu
@@ -0,0 +1,27 @@
+#include <cuda_runtime.h>
+
+// Headers CUDA
+
+//TODO: Si solo se manda y_true y y_pred
+__global__ void AccuaracyScore(float*, float*); 
+//TODO: Si solo se manda y_true y y_pred y normalize (pro defecto es TRUE)
+__global__ void AccuaracyScore(float*, float*, bool);
+//TODO: Si solo se manda y_true y y_pred, normalize (pro defecto es TRUE) y sample weight (array)
+__global__ void AccuaracyScore(float*, float*, bool, float*);
+//TODO: Si solo se manda y_true y y_pred y sample weight
+__global__ void AccuaracyScore(float*, float*, float*);
+
+// Headers C++
+
+//TODO: Hace un reporte de la siguiente forma:
+//               precision    recall  f1-score   support
+//
+//     class 0       0.67      1.00      0.80         2
+//     class 1       0.00      0.00      0.00         1
+//     class 2       1.00      0.50      0.67         2
+//
+//    accuracy                           0.60         5
+//   macro avg       0.56      0.50      0.49         5
+//weighted avg       0.67      0.60      0.59         5
+//
+void ClassificationReport(float*, float* /*, TargetNames*/ );
