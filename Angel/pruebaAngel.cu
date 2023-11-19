a16f07a28fb0745df452d73ed90cecb7d207bf42
AngelArriaga0503 <l22211357@gmail.com>
2023-11-19 15:13:51 -0800

    funciona n * m

--- Angel/pruebaAngel.cu
+++ Angel/pruebaAngel.cu
@@ -3,36 +3,62 @@
 #include <stdio.h>
 
 __global__ void kernel(float* mSemantica, float* targetValues, float* accuracyScore, int m){
-    if (mSemantica[threadIdx.y * m + threadIdx.x] == targetValues[threadIdx.x])
+    if (mSemantica[(gridDim.x * blockIdx.y + blockIdx.x) * (blockDim.x * blockDim.y) + (threadIdx.y * blockDim.x) + threadIdx.x] == targetValues[blockDim.x * blockIdx.x + threadIdx.x])
     {
         //printf("thread y mSemantica[%i][%i]: %f = target[%i]: %f\n", threadIdx.y, threadIdx.x, mSemantica[threadIdx.y * m + threadIdx.x], threadIdx.x, targetValues[threadIdx.x]);
-        atomicAdd(&accuracyScore[threadIdx.y], 1);
+        atomicAdd(&accuracyScore[blockDim.y * blockIdx.y + threadIdx.y], 1);
     }
     
-    if (threadIdx.x == m - 1){
-        // if (accuracyScore[threadIdx.y] > 0) printf ("%f\n",accuracyScore[threadIdx.y] / m);
-        printf("%i\n", threadIdx.y);
+    if (blockDim.x * blockIdx.x + threadIdx.x == m - 1){
+        //if (accuracyScore[threadIdx.y] > 0) printf ("%f\n",accuracyScore[threadIdx.y] / m);
+        // printf("%i\n", threadIdx.y);
     }
         
     
 }
 
-__global__ void kernel1(){
-}
+
 void Llenarvector(float* vector, int n, int m, int value){
     for (int i = 0; i < n * m; i++)
         vector[i] = i;
 }
 void Llenarmatriz(float* vector, int n, int m){
     for (int i = 0; i < m * n ; i++)
-        vector[i] = i; 
+        vector[i] = 1; 
 }
+
+
+
+void splitM(int y, int x, int& newY, int& newX, int& gridY,  int& gridX){
+    bool ok = true;
+    bool ok2 = true;
+    bool ok3 = true;
+    int multiploY = 1;
+    int multiploX = x;
+    while (ok){
+        if (x > pow (2, 31) - 1  && ok2){
+            printf("hola");
+            while (ok3){
+                multiploX--;
+                if ((x / multiploX) < pow (2, 31) - 1 && x % multiploX == 0) { ok = false; ok2 = false; }
+            }
+        }
+        if (((y / multiploY) * (x / multiploX)) < 1024 && (y % multiploY) == 0 ) { ok = false; }
+        else multiploY++;
+    }
+    newY = y / multiploY;
+    newX = x / multiploX;
+    gridY = (y + newY - 1) / newY;
+    gridX = (x + newX - 1) / newX;
+
+}
+
 
 int main(){
     float* targetValues; float* mSemantica; float* accuracyScore;
     float* targetValues_d; float* mSemantica_d; float* accuracyScore_d;
-    int n = 100;
-    int m = 10;
+    int n = 1024;
+    int m = 1024;
     targetValues = (float*)malloc(m * sizeof(float));
     mSemantica = (float*)malloc(n * m * sizeof(float));
     accuracyScore = (float*)malloc(n * sizeof(float));
@@ -47,17 +73,25 @@ int main(){
     cudaMemcpy(targetValues_d, targetValues, m * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(mSemantica_d, mSemantica, n * m * sizeof(float), cudaMemcpyHostToDevice);
 
+    int BdimY = 0;
+    int BdimX = 0;
 
-    dim3 block (m,n);
-    kernel<<<1,block>>>(mSemantica_d, targetValues_d, accuracyScore_d, m);
+    int GdimY = 0;
+    int GdimX = 0;
+
+
+    splitM(n, m, BdimY, BdimX, GdimY, GdimX);
+    dim3 block (BdimX,BdimY);
+    dim3 grid(GdimX,GdimY);
+    kernel<<<grid,block>>>(mSemantica_d, targetValues_d, accuracyScore_d, m);
     cudaDeviceSynchronize();
 
     cudaMemcpy(accuracyScore, accuracyScore_d, n * sizeof(float), cudaMemcpyDeviceToHost);
     
-    /*for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("Accuracy Score [%i]: %f\n", i, accuracyScore[i]);
-    }*/
+    }
     
 
 
