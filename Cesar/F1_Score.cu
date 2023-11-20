c7494ad5fef41e9739ea9497aee65f2ea1d3c27e
CesarLepeITT <L22212360@tijuana.tecnm.mx>
2023-11-20 11:11:17 -0800

    Inicio de desarrollo F1_Score

--- Cesar/F1_Score.cu
+++ Cesar/F1_Score.cu
@@ -1,31 +1,28 @@
 #include <cuda.h>
 #include <stdlib.h>
 #include <stdio.h>
-// TODO: Documentar el codigo
-// TODO: Probar un problema real con el codigo
-// TODO: Optimizar rendimiento general
 
-// Accuracy clasification score given a y_true and a semantic matrix
-// Parameters:
-//----------
-// float *y_pred: Matriz de ny x nx dimensiones que representa las predicciones
-// realizada por ny individuos con nx parametros.
-// float *y_true: Array de nx elementos que representa los valores esperados de las 
-// predicciones realizadas por los individuos
-// float *accuracy: Array de ny elementos que representa el accuracy de ny individuos
-// int nx: nx elementos
-// int ny: ny elementos
-__global__ void f1_score(float *y_true, float *y_pred, float *accuaracy, int nx, int ny)
+__global__ void f1_score(float *y_true, float *y_pred, float *f1_score, int nx, int ny, unsigned int *aux)
 {
     unsigned int ix = threadIdx.x + blockIdx.x * blockDim.x;
     unsigned int iy = threadIdx.y + blockIdx.y * blockDim.y;
     unsigned int tid = iy * nx + ix;
     unsigned int posx = tid - (iy * nx);
-
-    if (y_pred[tid] == y_true[posx] && tid < nx * ny)
+    
+    if (tid < nx * ny)
     {
-        float sum = 1 / (float)nx;
-        atomicAdd(&accuaracy[iy], sum);
+        if (y_pred[tid] == 1 && y_true[posx] == 1) // TP
+        {
+            //atomicAdd(&sumTP, 1);
+        }
+        if (y_pred[tid] == 1 && y_true[posx] == 0) // FP
+        {
+            //atomicAdd(&sumFP, 1);
+        }
+        if (y_pred[tid] == 0 && y_true[posx] == 1) // FN
+        {
+            //atomicAdd(&sumFN, 1);
+        }
     }
 }
 
@@ -76,20 +73,23 @@ int main()
     int nBytesPredictions = nm * sizeof(float);
     int nBytesTargetValues = nx * sizeof(float);
     int nBytesAccuracy = ny * sizeof(float);
+    int nBytesAux = ny * 3 * sizeof(unsigned int);
 
     // Host memory allocation
     float *predictions, *targetValues, *h_accuracy;
-
     h_accuracy = (float *)malloc(nBytesAccuracy);
     cudaMallocHost((void **)&predictions, nBytesPredictions);
     cudaMallocHost((void **)&targetValues, nBytesTargetValues);
 
     // Device memory allocation
     float *d_accuracy;
+    unsigned int *d_aux;    
     cudaMalloc((void **)&d_accuracy, nBytesAccuracy);
+    cudaMalloc((void**)&d_aux, nBytesAux);
 
     // Host memory initialization
     FillingMatrices(predictions, ny, nx);
+    cudaMemset(d_aux, 0, nBytesAux);
     Predictions(targetValues, nx, 1);
     VectorVacio(h_accuracy, 1, 0);
 
@@ -103,13 +103,13 @@ int main()
     dim3 block(dimx, dimy);
     dim3 grid((nx + block.x - 1) / block.x, (ny + block.y - 1) / block.y);
 
-    f1_score<<<grid, block>>>(targetValues, predictions, d_accuracy, nx, ny);
+    f1_score<<<grid, block>>>(targetValues, predictions, d_accuracy, nx, ny, d_aux);
     cudaDeviceSynchronize();
 
     // Memory transfer device to host
     cudaMemcpy(h_accuracy, d_accuracy, nBytesAccuracy, cudaMemcpyDeviceToHost);
 
-   // PrintVect(h_accuracy, ny);
+    // PrintVect(h_accuracy, ny);
 
     // Reset device
     cudaDeviceReset();
